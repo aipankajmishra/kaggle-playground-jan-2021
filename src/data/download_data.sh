7d15442e44a66eb64e42d49eaa1a5da85d94acce
Pankaj Mishra <88949597+aipankajmishra@users.noreply.github.com>
2021-09-06 23:37:18 +0200

    Modification to makefile

--- src/data/download_data.sh
+++ src/data/download_data.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+rm -rf data/raw/*
 kaggle competitions download -p data/raw -c tabular-playground-series-jan-2021
-unzip data/raw/*.zip
+unzip data/raw/*.zip -d data/raw/
 rm -rf data/raw/*.zip
+
+train_file=$(find data/raw/ -type f -iname '*train*')
+test_file=$(find data/raw/ -type f -iname '*test*')
+sample_file=$(find data/raw/ -type f -iname '*sample*') 
+
+
+mv $train_file data/raw/raw_train.csv
+mv $test_file data/raw/raw_test.csv
+mv $sample_file data/raw/raw_sample.csv
+
